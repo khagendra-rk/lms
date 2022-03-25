7cf00a7e7ec8bbf37c92f7ea0b2485cb250c690e
RamKrishna <mahatokrishnaram@gmail.com>
2022-03-25 19:40:40 +0545

    Controller & Routes are created

---
+++ app/Http/Controllers/Borrow/BorrowController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Borrow;
+
+use App\Http\Controllers\Controller;
+use App\Models\Borrow;
+use Illuminate\Http\Request;
+
+class BorrowController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Borrow  $borrow
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Borrow $borrow)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Borrow  $borrow
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Borrow $borrow)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Borrow  $borrow
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Borrow $borrow)
+    {
+        //
+    }
+}
